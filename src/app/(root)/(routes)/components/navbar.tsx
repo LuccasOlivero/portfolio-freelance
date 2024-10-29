f0c96e18913c6e6d8d92d4a3f25bfc663cdcfdf0
lucas <lucas.chorolqui@gmail.com>
2024-10-29 19:04:08 -0300

    setup

---
+++ src/app/(root)/(routes)/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+    <nav className="w-full z-[99] h-[3.6rem] px-10 backdrop-blur-sm border-l-[2px] border-r-[2px] border-b-[2px] flex  items-center justify-between sm:px-8 xs:px-4">
+      LOGO
+      <h1>since 2022</h1>
+      BURGER
+    </nav>
+  );
+}
---
+++ src/app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import NavBar from "./components/navbar";
+
+export default function Main() {
+  return (
+    <div className="container mx-auto">
+      <NavBar />
+    </div>
+  );
+}
